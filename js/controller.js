d131040db3560770e1a93fb753112506bc08485a
lollyborch <lilly.borchardt@gmail.com>
2016-09-13 20:54:51 +1000

    added w3 styling and incorporated dummy search

--- js/controller.js
+++ js/controller.js
@@ -1,27 +1,57 @@
+//Search filter from
+//http://stackoverflow.com/questions/31008677/search-using-filter-only-after-click-in-angular
+
+//Clean URL from
+//http://stackoverflow.com/questions/5343288/get-the-domain-and-page-name-from-a-string-url
+
 var app = angular.module('myApp', []);
 
-app.controller('newsBias', function($scope) {  
+app.controller('newsBias', function($scope) { 
+
   $scope.newsSites = [
-    {url:'brisbanetime.com',name:'Brisbane Times',bias:'one'},
+    {url:'brisbanetimes.com',name:'Brisbane Times',bias:'one'},
     {url:'abc.net.au',name:'ABC News',bias:'two'},
     {url:'theconversation.com',name:'The Conversation',bias:'three'}
   ];
-});
-
-//http://www.primaryobjects.com/2012/11/19/parsing-hostname-and-domain-from-a-url-with-javascript/
-
-function getHostName(urlInput) {
-  url = document.getElementById(urlInput).value;
-    var matchProtocol = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-  console.log(matchProtocol);
-    if (matchProtocol != null && matchProtocol.length > 2 && typeof matchProtocol[2] === 'string' && matchProtocol[2].length > 0) {
-    var result = matchProtocol[2];
-    document.getElementById('demo').innerHTML = result;
-    }
-    else {
-        return null;
-    }
+  $scope.domain = "";
+  $scope.page = "";
   
-}
+  $scope.cleanURL=function(url){
+    
+    //remove "http://"
+    if (url.indexOf("http://") == 0) {
+        url = url.substr(7);
+    }
+  //remove "https://"
+    if (url.indexOf("https://") == 0) {
+        url = url.substr(8);
+    }
+    //remove "www."
+    if (url.indexOf("www.") == 0) {
+        url = url.substr(4);
+    }
+    $scope.domain = url.split('/')[0].split('.')[0]
+    if (url.split('/').length > 1) {
+        $scope.page = url.split('/')[1].split('.')[0];
+    }
+    //document.write("domain : " + domain + (page == "" ? "" : " page : " + page) + "<br/>");
+    console.log("domain : " + $scope.domain + ($scope.page == "" ? "" : " page : " + $scope.page));
 
-//document.getElementById("demo").innerHTML = getHostName('http://www.brisbanetimes.com.au/business/mining-and-resources/clive-palmer-had-full-authority-of-queensland-nickel-court-20160909-grcl09.html');
+  }
+  //$scope.cleanURL("www.myWebSite.com"); // domain : myWebSite
+  //$scope.cleanURL("http://myWebSite.com"); // domain : myWebSite
+  //$scope.cleanURL("myWebSite.com/xxx.html"); // domain : myWebSite page : xxx
+  //$scope.cleanURL("https://www.myWebSite.com/meowbeans.html"); // domain : myWebSite page : xxx
+  
+  
+    $scope.searchURL=function(urlInput){
+        var url = document.getElementById(urlInput).value;
+        $scope.cleanURL(url);
+      
+        $scope.searchQuery = angular.copy($scope.domain);
+        //$scope.searchQuery = angular.copy($scope.newsQuery);
+        $scope.urlToFilter=$scope.newsSites;
+        $scope.searchResult=true;
+
+    }
+  });
